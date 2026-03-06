2a765734868cbac90491506167f93ee4ca549e8d
Aashish Maharjan <aashishmah177@gmail.com>
2026-03-05 18:19:21 -0600

    added GAT and GraphSage

--- src/model.py
+++ src/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, GATConv, SAGEConv
 
 class GCN(torch.nn.Module):
     def __init__(self, num_features, hidden_channels, num_classes):
@@ -25,4 +25,54 @@ class GCN(torch.nn.Module):
         # 4. Second message Passing step
         x = self.conv2(x, edge_index)
 
+        return x
+    
+class GAT(torch.nn.Module):
+    """Graph Attention Network"""
+    def __init__(self, num_features, hidden_channels, num_classes, heads=8, dropout=0.6):
+        super(GAT, self).__init__()
+        self.dropout = dropout
+        
+        # First GAT layer with multi-head attention
+        # heads=8 means 8 attention heads, output is concatenated
+        self.conv1 = GATConv(
+            num_features, 
+            hidden_channels, 
+            heads=heads, 
+            dropout=dropout
+        )
+        
+        # Second GAT layer
+        # Input size is hidden_channels * heads (concatenated from previous layer)
+        # Output single head, concat=False means average instead of concatenate
+        self.conv2 = GATConv(
+            hidden_channels * heads, 
+            num_classes, 
+            heads=1, 
+            concat=False, 
+            dropout=dropout
+        )
+        
+    def forward(self, x, edge_index):
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv1(x, edge_index)
+        x = F.elu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x
+
+
+class GraphSAGE(torch.nn.Module):
+    """GraphSAGE Network"""
+    def __init__(self, num_features, hidden_channels, num_classes, dropout=0.5):
+        super(GraphSAGE, self).__init__()
+        self.conv1 = SAGEConv(num_features, hidden_channels)
+        self.conv2 = SAGEConv(hidden_channels, num_classes)
+        self.dropout = dropout
+        
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv2(x, edge_index)
         return x
